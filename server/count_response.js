7b14de0b96c75de35136821d23c92ae697135a12
Christos Vontas <christos@redsift.io>
2017-12-12 19:18:39 +0000

    added counter for forensic store

---
+++ server/count_response.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = function (got) {
+  const inData = got.in;
+  console.log("count_response triggered!")
+
+  return {
+    name: 'api_rpc',
+    key: got.get[0].value.toString(),
+    value: {
+      status_code: 200,
+      body: Buffer.from('forensic-st=' + inData.data.length).toString('base64')
+    }
+  }
+}
---
+++ server/staremiter.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = function (got) {
+  console.log("staremiter triggered!")
+  return {name: 'forensics-counter-st', key: '*/*'}
+}
