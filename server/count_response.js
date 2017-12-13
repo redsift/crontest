6ea413b6ea93bc6f182edcf5715f4cbbfba6cd3a
Christos Vontas <christos@redsift.io>
2017-12-13 16:24:49 +0000

    fix structure for get

--- server/count_response.js
+++ server/count_response.js
@@ -3,10 +3,10 @@
 module.exports = function (got) {
   const inData = got.in;
   console.log("count_response triggered!")
-
+  console.log(got.get[0].data)
   return {
     name: 'api_rpc',
-    key: got.get[0].value.toString(),
+    key: got.get[0].data[0].value.toString(),
     value: {
       status_code: 200,
       body: Buffer.from('forensic-st=' + inData.data.length).toString('base64')
