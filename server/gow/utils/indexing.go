e3337a885f0c82e1a17f5d203b7cf164bedda806
Christos Vontas <christos@redsift.io>
2017-12-14 15:49:47 +0000

    add log_file_to_roll=300s

--- server/gow/utils/indexing.go
+++ server/gow/utils/indexing.go
@@ -91,6 +91,7 @@ func openToWriteOrCreate(name, indexPath string) (bleve.Index, error) {
 	cfg := map[string]interface{}{
 			"enable_statistics": true,
 			"keep_log_file_num": 100,
+			"log_file_time_to_roll": 300,
 			// "prepare_for_bulk_load": true,
 		}
 	idx, err := bleve.OpenUsing(indexPath, cfg)
